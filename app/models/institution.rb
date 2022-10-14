3c2e08d4e7f755c2a3ecbc92e4fd073212be4131
marvynribeiro <marvynribeiro@gmail.com>
2022-10-13 22:26:23 -0300

    allow institution.type_of to be blank

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -7,8 +7,10 @@ class Institution < ApplicationRecord
   enum type_of: TYPE_OF_CATEGORIES
 
   has_many :enrollments
-  validates :name, :cnpj, :type_of, presence: true
+  validates :name, :cnpj, presence: true
   validates :name, :cnpj, uniqueness: true
   validates :cnpj, numericality: { only_integer: true }
-  validates_inclusion_of :type_of, in: Institution.type_ofs.keys
+  validates_inclusion_of :type_of,
+                          in: Institution.type_ofs.keys,
+                          allow_blank: true
 end
