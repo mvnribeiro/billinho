ef78713e0483d7a77f7e4d9241d147f3b731acc7
Marvyn Ribeiro <marvynribeiro@gmail.com>
2022-10-24 10:49:21 -0300

    Update enrollment.rb

--- spec/factories/enrollment.rb
+++ spec/factories/enrollment.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     institution
     student
     course_name { ['Biologia', 'Educação Física', 'Letras', 'Matemática', 'Ciência da Computação', 'Química', 'Matemática', 'Jornalismo', 'Filosofia'].sample }
-    total_value { rand(100.0..100_000.0) }
+    total_value { rand(10_000.0..100_000.0) }
     total_invoices { [1, 6, 12, 18, 24, 30].sample }
     due_day { rand(1..31) }
   end
